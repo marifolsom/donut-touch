44cd3e6707609baca9c329a81e70e1595e9d4770
marifolsom <mfolsom1@fordham.edu>
2018-02-28 16:28:26 -0500

    started psuedo coding

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/////////////// HOW TO PLAY ///////////////
+// Enter the game website
+// Enter username -- form input
+// Click 'Play!' -- button linked to levels.html
+// Select a level -- each level button is linked to its respective level html file
+// Open selected level file
+// Objects are already moving, items are already placed and ready to go
+// User's cursor enters the start zone and clicks
+// On click, level timer starts counting (up or down?)
+// If cursor enters into obstacle element, everything turns red -- 'GAME OVER' pop up
+// If cursor makes it all the way to end zone and clicks, timer stops -- 'Level cleared!' pop up
+// User has the option to play the level over (if they didn't win or want to get a higher score), -- link to current
+// return back to the menu, or if they cleared the level, they can advance to the next level -- link to next
+// Once the player has cleared all (five?) levels, they've beat the game! -- 'Congratulations' page
