1cee66ac19b0e4abecd630ef3a1bb19345d433f0
marifolsom <mfolsom1@fordham.edu>
2018-03-02 19:41:58 -0500

    added some more jquery

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ console.log('app.js linked!');
 // Objects are already moving, items are already placed and ready to go
 // User's cursor enters the start zone and clicks
 // On click, level timer starts counting (up or down?)
-// If cursor enters into obstacle element, everything turns red -- 'GAME OVER' pop-up
+// If cursor enters into obstacle element or out of the game screen, everything turns red -- 'GAME OVER' pop-up
 // If cursor makes it all the way to end zone and clicks, timer stops -- 'Level cleared!' pop-up
 // User has the option to play the level over (if they didn't win or want to get a higher score), return back to the menu, or if they cleared the level, they can advance to the next level -- link to next
 // Once the player has cleared all (five?) levels, they've beat the game! -- 'Congratulations' page
@@ -43,7 +43,6 @@ let donuts = document.querySelectorAll('.donut');
 // });
 
 
-
 // Make a function that creates a pop up
 let makePopUp = () => {
   // If pop up already exists, do nothing
@@ -60,11 +59,44 @@ let makePopUp = () => {
   }
 }
 
+
 // Make a function that creates the elements for the levels page
 let makeLevelsPage = () => {
   $levelsPage = $('<a href="index.html"><h1>CURSOR GAME</h1></a><div class="levels"><p>Select a level:</p><a class="button one">LEVEL ONE</a><a class="button two">LEVEL TWO</a><a class="button three">LEVEL THREE</a><a class="button four">LEVEL FOUR</a><a class="button five">LEVEL FIVE</a></div>')
   $main.html($levelsPage);
+  // Add event listener to level one button
+  let oneButton = document.querySelector('.one');
+  oneButton.addEventListener (
+    'click',
+    makeLevelOne
+  );
+  // Add event listener to level two button
+  let twoButton = document.querySelector('.two');
+  twoButton.addEventListener (
+    'click',
+    makeLevelTwo
+  );
+  // Add event listener to level three button
+  let threeButton = document.querySelector('.three');
+  threeButton.addEventListener (
+    'click',
+    makeLevelThree
+  );
+  // // Add event listener to level four button
+  // let fourButton = document.querySelector('.four');
+  // fourButton.addEventListener (
+  //   'click',
+  //   makeLevelOne
+  // );
+  // // Add event listener to level five button
+  // let fiveButton = document.querySelector('.five');
+  // fiveButton.addEventListener (
+  //   'click',
+  //   makeLevelOne
+  // );
 }
+
+
 // Add event listener to play button
 let playButton = document.querySelector('.play-button');
 playButton.addEventListener (
@@ -76,62 +108,46 @@ playButton.addEventListener (
 // Make a function that creates all the elements for level one elements
 let makeLevelOne = () => {
   let $levelOnePage = $('<div class="level-one"><nav class="information-bar"><a href="index.html"><h2 class="title">CURSOR GAME</h2></a><a href="levels.html"><h2 class="title">LEVEL ONE</h2></a><h2 class="score-content" id="username"></h2><h2 class="score-content" id="user-score">0000</h2></nav><section class="game-screen"><span class="start-end-zone" id="start-zone">START</span><span class="obstacle"></span><span class="obstacle"></span><span class="obstacle"></span><span class="obstacle"></span><span class="obstacle"></span><span class="obstacle"></span><img class="donut" src="img/donut.png" alt="donut icon" points="10"><img class="donut" src="img/donut.png" alt="donut icon" points="10"><span class="start-end-zone" id="end-zone">END</span></section></div>');
-  $main.html($levelOnePage);
+  $body.html($levelOnePage);
+  // Add event listener to start zone
+  startZone.addEventListener (
+    'click', startGame
+  );
 }
-// Add event listener to level one button
-let oneButton = document.querySelector('.one');
-oneButton.addEventListener (
-  'click',
-  makeLevelOne
-);
 
 
 // Make a function that creates all the elements for level two
 let makeLevelTwo = () => {
-  let $levelTwoPage = $('<div class="level-two"><nav class="information-bar"><a href="index.html"><h2 class="title">CURSOR GAME</h2></a><a href="levels.html"><h2 class="title">LEVEL TWO</h2></a><h2 class="score-content" id="username"></h2><h2 class="score-content" id="user-score">0000</h2></nav><section class="game-screen"> <span class="start-end-zone" id="start-zone">START</span> <span class="obstacle"></span><span class="obstacle"></span><span class="obstacle"></span><span class="obstacle"></span><img class="donut" src="img/donut.png" alt="donut icon" points="10"><img class="donut" src="img/donut.png" alt="donut icon" points="10"><img class="donut" src="img/donut.png" alt="donut icon" points="10"><span class="start-end-zone" id="end-zone">END</span></section></div>')
+  let $levelTwoPage = $('<div class="level-two"><nav class="information-bar"><a href="index.html"><h2 class="title">CURSOR GAME</h2></a><a href="levels.html"><h2 class="title">LEVEL TWO</h2></a><h2 class="score-content" id="username"></h2><h2 class="score-content" id="user-score">0000</h2></nav><section class="game-screen"> <span class="start-end-zone" id="start-zone">START</span> <span class="obstacle"></span><span class="obstacle"></span><span class="obstacle"></span><span class="obstacle"></span><img class="donut" src="img/donut.png" alt="donut icon" points="10"><img class="donut" src="img/donut.png" alt="donut icon" points="10"><img class="donut" src="img/donut.png" alt="donut icon" points="10"><span class="start-end-zone" id="end-zone">END</span></section></div>');
+  $body.html($levelTwoPage);
+  // Add event listener to start zone
+  startZone.addEventListener (
+    'click', startGame
+  );
 }
-// Add event listener to level two button
-let twoButton = document.querySelector('.two');
-twoButton.addEventListener (
-  'click',
-  makeLevelTwo
-);
 
 
 // Make a function that creates all the elements for level three
 let makeLevelThree = () => {
-  let $levelThreePage = $('<div class="level-three"><nav class="information-bar"><a href="index.html"><h2 class="title">CURSOR GAME</h2></a><a href="levels.html"><h2 class="title">LEVEL THREE</h2></a><h2 class="score-content" id="username"></h2><h2 class="score-content" id="user-score">0000</h2></nav><section class="game-screen"><span class="start-end-zone" id="start-zone">START</span><span class="obstacle"></span><span class="obstacle"></span><span class="obstacle"></span><span class="obstacle"></span><img class="donut" src="img/donut.png" alt="donut icon" points="10"><img class="donut" src="img/donut.png" alt="donut icon" points="10"><img class="donut" src="img/donut.png" alt="donut icon" points="10"><span class="start-end-zone" id="end-zone">END</span></section></div>')
+  let $levelThreePage = $('<div class="level-three"><nav class="information-bar"><a href="index.html"><h2 class="title">CURSOR GAME</h2></a><a href="levels.html"><h2 class="title">LEVEL THREE</h2></a><h2 class="score-content" id="username"></h2><h2 class="score-content" id="user-score">0000</h2></nav><section class="game-screen"><span class="start-end-zone" id="start-zone">START</span><span class="obstacle"></span><span class="obstacle"></span><span class="obstacle"></span><span class="obstacle"></span><img class="donut" src="img/donut.png" alt="donut icon" points="10"><img class="donut" src="img/donut.png" alt="donut icon" points="10"><img class="donut" src="img/donut.png" alt="donut icon" points="10"><span class="start-end-zone" id="end-zone">END</span></section></div>');
+  $body.html($levelThreePage);
+  // Add event listener to start zone
+  startZone.addEventListener (
+    'click', startGame
+  );
 }
-// Add event listener to level three button
-let threeButton = document.querySelector('.three');
-threeButton.addEventListener (
-  'click',
-  makeLevelthree
-);
 
 
 // Make a function that creates all the elements for level four
 let makeLevelFour = () => {
 
 }
-// // Add event listener to level four button
-// let fourButton = document.querySelector('.four');
-// fourButton.addEventListener (
-//   'click',
-//   makeLevelOne
-// );
 
 
 // Make a function that creates all the elements for level five
 let makeLevelFive = () => {
 
 }
-// // Add event listener to level five button
-// let fiveButton = document.querySelector('.five');
-// fiveButton.addEventListener (
-//   'click',
-//   makeLevelOne
-// );
 
 
 // Make a function that when the user's cursor enters into the obstacle elements, the objects and game screen turn red and triggers the 'GAME OVER' pop-up
@@ -221,9 +237,3 @@ let getDonutScore = (evt) => {
     'mouseenter', getDonutScore
   );
 }
-
-
-// Add event listener to start zone
-startZone.addEventListener (
-  'click', startGame
-);
